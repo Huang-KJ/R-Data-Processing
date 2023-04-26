b18f8ad777e81a01890844fc77c63f283e20faba
Huang-Kyle <w04268@gmail.com>
2023-04-26 12:16:43 +0800

    update license

--- Weighting/Raking_社會變遷.R
+++ Weighting/Raking_社會變遷.R
@@ -1,6 +1,6 @@
-################
+##################
 # 多變項反覆加權 #
-################
+##################
 library(car)
 library(XML)
 library(haven)
@@ -17,14 +17,15 @@ tscs201 <- read_dta('tscs201.dta') |> setDT()
 tscs201 <- tscs201[, Sex := factor(x = v1, levels = c(1:2), labels = c("男", "女"))]
 tscs201 <- apply_labels(data = tscs201, Sex = "性別")
 
-tscs201 <- (copy(tscs201)[v2y < 100, Age := 110 - v2y]
+tscs201 <- (copy(tscs201)[v2y < 100, Age := 109 - v2y]
                          [v2y > 100, Age := grep("[0-9]", v2r)]
                          [, Age := Recode(Age, recodes = '18:29="18-29歲"; 30:39="30-39歲";
                                                           40:49="40-49歲"; 50:59="50-59歲";
-                                                          60:69="60-69歲"; else="70歲以上"')])
+                                                          60:69="60-69歲"; else="70歲以上"')]
+                         [v118 != 97, ])
 tscs201 <- apply_labels(data = tscs201, Age = "年齡")
 
-Sample <- tscs201[, .(Sex, Age, id = c(1:nrow(tscs201)))]
+Sample <- tscs201[, .(Sex, Age, License = v118, id = c(1:nrow(tscs201)))]
 
 #匯入人口母體資料#
 setwd('/Users/kyle/Desktop/NCCU/社會調查師/R-Data-Processing/DataSets')
@@ -65,14 +66,62 @@ Sample$weights <- weights(raking)
 chisq.test(wpct(Sample$Sex, Sample$weights), p = as.numeric(sex[2, ]))
 chisq.test(wpct(Sample$Age, Sample$weights), p = round(as.numeric(age[2, ]), 2))
 
+Sample <- Sample[, ":="(Sex = as.character(Sex), Age = as.character(Age))]
+
+sex3 <- prop.table(table(Sample$Sex)) |> data.frame() |> t()
+s <- as.numeric(sex[2,]) / as.numeric(sex3[2,])
+sexW <- data.frame(Sex = c('男', '女'), WS = s)
+Sample <- dplyr::left_join(Sample, sexW)
+
+chisq.test(wpct(Sample$Sex, Sample$WS), p = as.numeric(sex[2, ]))
+chisq.test(wpct(Sample$Age, Sample$WS), p = round(as.numeric(age[2, ]), 2))
+
+SexAgeCount <- Sample[,.(.N), by = .(Age, Sex)]
+AgeProb <- dplyr::left_join(SexAgeCount, sexW)
+AgeProb <- AgeProb[, N := N * WS][, .(N = sum(N)), by = .(Age)][, ProbA := N / sum(N)][]
+
+A <- Population[, .(人口數比例 = 人口數 / sum(人口數), Age)][, .(FreqA = sum(人口數比例)), by = .(Age)]
+ageW <- dplyr::left_join(AgeProb, A)
+ageW <- ageW[, .(Age, WA = FreqA / ProbA)]
+
+Sample <- dplyr::left_join(Sample, ageW)
+
+chisq.test(wpct(Sample$Sex, Sample$WA), p = as.numeric(sex[2, ]))
+chisq.test(wpct(Sample$Age, Sample$WA), p = round(as.numeric(age[2, ]), 2))
+
+#v41#
+prop.table(table(tscs201$v41))
+wpct(Sample$weights)
+
 #檢視駕照比例#
 setwd('/Users/kyle/Desktop/NCCU/社會調查師/R-Data-Processing/DataSets')
-license <- xmlToDataFrame('領有駕駛執照人數.XML') |> setDT()
-license <- license[Category1Title == '小型車普通駕駛人' & Category2Title %in% c('男', '女') & Category3Title != '計', ]
+license <- xmlToDataFrame('領有駕駛執照人數2.XML') |> setDT()
+license <- (license[Category1Title == '小型車普通駕駛人' & Category2Title %in% c('男', '女') & Category3Title != '計', ]
+                   [, ":="(Sex = Category2Title,
+                           Age = Recode(Category3Title, "c('未滿20歲', '20-未滿25歲', '25-未滿30歲')='18-29歲';
+                                                         c('30-未滿35歲', '35-未滿40歲')='30-39歲';
+                                                         c('40-未滿45歲', '45-未滿50歲')='40-49歲';
+                                                         c('50-未滿55歲', '55-未滿60歲')='50-59歲';
+                                                         '60歲以上'='60歲以上'"))])
+
+Sample$Age2 <- ifelse(Sample$Age %in% c('60-69歲', '70歲以上'), '60歲以上', Sample$Age)
+
+license <- license[Period == '11000', .(人數 = sum(as.numeric(Val))), keyby = .(Sex, Age)][, 比例 := 人數 / sum(人數) * 1854][]
+
+Sample2 <- Sample[License == 1, .(人數 = sum(weights)), keyby = .(Sex, Age2)][]
+
+chisq.test(license$比例, Sample2$人數)
+
+l.Unweighted <- table(tscs201$v118)
+l.sample <- wpct(tscs201$v118, Sample$weights) * 1854
+
+有駕照比例 <- sum(license$人數) / sum(Population$人口數)
+沒駕照比例 <- (sum(Population$人口數) - sum(license$人數)) / sum(Population$人口數)
+
+l.stat <- data.frame(a = 有駕照比例 * 1854,  b = 沒駕照比例 * 1854)
+
+chisq.test(l.Unweighted, p = c(有駕照比例, 沒駕照比例))
+chisq.test(l.sample, p = c(有駕照比例, 沒駕照比例))
 
 
 #devtools::install_github("rstudio/addinexamples", type = "source")
-
-prop.table(table(tscs201$v118))
-wpct(tscs201$v118, Sample$weights)
-
